515e63ee56554e07e646f0b9eebfa8fa9f1c5c6c
zhengpengxie <zhengpengxie56@gmail.com>
2025-07-06 02:19:09 +0000

    Deploying to gh-pages from @ zhengpengxie/zhengpengxie.github.io@5623cdea9d22873da4c04995fbd544233b96b823 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,28 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
