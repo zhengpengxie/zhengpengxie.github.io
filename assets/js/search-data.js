4c100c1599eef9ad18fbe1dcddc6032bf0b3a8c7
zhengpengxie <zhengpengxie56@gmail.com>
2025-07-06 03:06:04 +0000

    Deploying to gh-pages from @ zhengpengxie/zhengpengxie.github.io@a5638d88417ff9720c66b997ae22b15a5990aa38 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,8 +384,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-simple-policy-optimization-is-accepted-by-icml-2025-smile",
+          title: 'Simple Policy Optimization is accepted by ICML 2025! :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
